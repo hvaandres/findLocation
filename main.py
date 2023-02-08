9521f18f279406259ad108fe1bfd2a912b6f929e
hvaandres <hvaandres@icloud.com>
2023-02-07 17:25:02 -0700

    first commit

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from phonenumbers import geocoder
 from phonenumbers import carrier
 from phonenumbers import carrier
 
-key = ["Add_your_token_in_here"]
+authentication_key = ["Add_your_token_in_here"]
 
 sanNumber = phonenumbers.parse(number)
-yourLocation = geocoder.description_for_number(sanNumber, "en")
-service_provider = phonenumbers.parse(number)
+myLocation = geocoder.description_for_number(sanNumber, "en")
+my_service_provider = phonenumbers.parse(number)
 
-geocoder = OpenCageGeocode(key)
+geocoder = OpenCageGeocode(authentication_key)
 
-query = str(yourLocation)
+query = str(myLocation)
 results = geocoder.geocode(query)
 
 lat = results[0]['geometry']['lat']
@@ -21,11 +21,11 @@ lng = results[0]['geometry']['lng']
 
 myMap = folium.Map(location=[lat, lng], zoom_start = 9)
 
-folium.Marker([lat, lng],popup= yourLocation).add_to((myMap))
+folium.Marker([lat, lng],popup= myLocation).add_to((myMap))
 
 
-print(yourLocation)
-print(carrier.name_for_number(service_provider, "en"))
+print(myLocation)
+print(carrier.name_for_number(my_service_provider, "en"))
 print(results)
 print(lat, lng)
 
